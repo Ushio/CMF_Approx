fb07e1dea0264991dd2e6e4f126231b1455017cc
ushiostarfish <ushiostarfish@gmail.com>
2025-09-27 15:35:05 +0900

    train and viewer implementation

--- main.cpp
+++ main.cpp
@@ -1,9 +1,83 @@
 ﻿#include "pr.hpp"
 #include <iostream>
 #include <memory>
+#include <fstream>
+#include <sstream>
+
+inline float asymmetric_gaussian(float x, float mean, float sigma, float a) {
+    float k = (x - mean) / (sigma + a * (x - mean));
+    return expf(-k * k);
+}
+inline float cmf_x_CIE_2006_10deg(float x) {
+    float a = 0.42f * asymmetric_gaussian(x, 445.5793762207031f, 31.10230255126953f, 0.06199074536561966f);
+    float b = 1.16f * asymmetric_gaussian(x, 594.541748046875f, 48.5798225402832f, -0.04986543208360672f);
+    return a + b - a * b * 42.51573944091797f;
+}
+inline float cmf_y_CIE_2006_10deg(float x) {
+    return 1.0f * asymmetric_gaussian(x, 556.8382568359375f, 66.54190826416016f, -0.026490915566682816f);
+}
+inline float cmf_z_CIE_2006_10deg(float x) {
+    return 2.146832f * asymmetric_gaussian(x, 445.9251708984375f, 30.91781997680664f, 0.08379142731428146f);
+}
+
+// importance sampling
+inline float logistic_pdf(float x, float s)
+{
+    float k = expf(-fabsf(x) / s);
+    return s * k / ((1.0 + k) * (1.0 + k));
+}
+inline float logistic_cdf(float x, float s)
+{
+    return 1.0f / (1.0f + expf(-x / s));
+}
+inline float inverse_logistic_cdf(float u, float s)
+{
+    if (0.99999994f < u) { u = 0.99999994f; }
+    if (u < 1.175494351e-38f) { u = 1.175494351e-38f; }
+    return -s * logf(1.0f / u - 1.0f);
+}
+inline float trimmed_logistic_pdf(float x, float s, float a, float b)
+{
+    return logistic_pdf(x, s) / (logistic_cdf(b, s) - logistic_cdf(a, s));
+}
+
+inline float cmf_y_pdf_CIE_2006_10deg(float x) {
+    float sx = x - 554.270751953125f;
+    float s = 26.879621505737305f;
+    float a = -164.270751953125;
+    float b = 275.729248046875;
+    return logistic_pdf(sx, 26.879621505737305f) / (logistic_cdf(b, s) - logistic_cdf(a, s));
+}
+
+inline float cmf_y_sample_CIE_2006_10deg(float u) {
+    float s = 26.879621505737305f;
+    float a = -164.270751953125;
+    float b = 275.729248046875;
+    float Pa = logistic_cdf(a, s);
+    float Pb = logistic_cdf(b, s);
+    return inverse_logistic_cdf(Pa + (Pb - Pa) * u, s) + 554.270751953125f;
+}
 
 int main() {
     using namespace pr;
+
+    std::vector<float> wavelength;
+    std::vector<float> Xs;
+    std::vector<float> Ys;
+    std::vector<float> Zs;
+
+    std::ifstream csv("../lin2012xyz10e_1_7sf.csv");
+    std::string line;
+    while (std::getline(csv, line)) {
+        std::stringstream ss(line);
+
+        float wave, x, y, z;
+        sscanf(line.c_str(), "%f,%f,%f,%f", &wave, &x, &y, &z);
+        wavelength.push_back(wave);
+        Xs.push_back(x);
+        Ys.push_back(y);
+        Zs.push_back(z);
+    }
 
     Config config;
     config.ScreenWidth = 1920;
@@ -12,10 +86,18 @@ int main() {
     Initialize(config);
 
     Camera3D camera;
-    camera.origin = { 0, 0, 10 };
-    camera.lookat = { 0, 0, 0 };
+    camera.origin = { 5, 0, 10 };
+    camera.lookat = { 5, 0, 0 };
 
     double e = GetElapsedTime();
+
+    bool showOriginalData = false;
+    bool showX = true;
+    bool showY = true;
+    bool showZ = true;
+
+    bool showPDF = false;
+    bool showSampledHistogram = false;
 
     while (pr::NextFrame() == false) {
         if (IsImGuiUsingMouse() == false) {
@@ -31,6 +113,107 @@ int main() {
         DrawGrid(GridAxis::XY, 1.0f, 20, { 128, 128, 128 });
         DrawXYZAxis(1.0f);
 
+        for (int nm = 0; nm <= 800; nm += 100)
+        {
+            float x = nm / 100.0f;
+            DrawText({ x, 0.0f, 0.0f }, std::to_string(nm) + " nm", 12);
+        }
+
+        // Plot
+        if (showOriginalData)
+        {
+            for (int i = 0; i < wavelength.size(); i++)
+            {
+                float nm = wavelength[i];
+                float x = nm / 100.0f;
+                DrawPoint({ x, Xs[i], 0.0f }, { 128, 0, 0 }, 4);
+                DrawPoint({ x, Ys[i], 0.0f }, { 0, 128, 0 }, 4);
+                DrawPoint({ x, Zs[i], 0.0f }, { 0, 0, 128 }, 4);
+            }
+        }
+
+        // Fitted
+        int N = 1000;
+
+        if (showX)
+        {
+            PrimBegin(PrimitiveMode::LineStrip, 1);
+            for (int i = 0; i < N; i++)
+            {
+                float nm = glm::mix(390.0f, 830.0f, (float)i / N);
+                float x = nm / 100.0f;
+                PrimVertex({ x, cmf_x_CIE_2006_10deg(nm), 0 }, { 255, 0, 0 });
+            }
+            PrimEnd();
+        }
+
+        if (showY)
+        {
+            PrimBegin(PrimitiveMode::LineStrip, 1);
+            for (int i = 0; i < N; i++)
+            {
+                float nm = glm::mix(390.0f, 830.0f, (float)i / N);
+                float x = nm / 100.0f;
+                PrimVertex({ x, cmf_y_CIE_2006_10deg(nm), 0 }, { 0, 255, 0 });
+            }
+            PrimEnd();
+        }
+
+        if (showZ)
+        {
+            PrimBegin(PrimitiveMode::LineStrip, 1);
+            for (int i = 0; i < N; i++)
+            {
+                float nm = glm::mix(390.0f, 830.0f, (float)i / N);
+                float x = nm / 100.0f;
+                PrimVertex({ x, cmf_z_CIE_2006_10deg(nm), 0 }, { 0, 0, 255 });
+            }
+            PrimEnd();
+        }
+
+        // PDF
+        static float pdf_view_scale = 0.2f;
+
+        if (showPDF)
+        {
+            PrimBegin(PrimitiveMode::LineStrip, 1);
+            for (int i = 0; i < N; i++)
+            {
+                float nm = glm::mix(390.0f, 830.0f, (float)i / N);
+                float x = nm / 100.0f;
+                PrimVertex({ x, cmf_y_pdf_CIE_2006_10deg(nm) * pdf_view_scale, 0 }, { 255, 255, 255 });
+            }
+            PrimEnd();
+        }
+
+        // sampling
+        static std::vector<int> buckets(900);
+        static int maxCount = 0;
+        if (showSampledHistogram)
+        {
+            PCG rng;
+
+            if (maxCount == 0)
+            {
+                for (int i = 0; i < 100000000; i++)
+                {
+                    float sampled = cmf_y_sample_CIE_2006_10deg(rng.uniformf());
+                    int index = (int)roundf(sampled);
+                    int cur = ++buckets[index];
+                    maxCount = std::max(maxCount, cur);
+                }
+            }
+
+            PrimBegin(PrimitiveMode::LineStrip, 2);
+            for (int nm = 0; nm < buckets.size(); nm++)
+            {
+                float x = nm / 100.0f;
+                float y = (float)buckets[nm] / maxCount;
+                PrimVertex({ x, y, 0 }, { 255, 255, 0 });
+            }
+            PrimEnd();
+        }
+
         PopGraphicState();
         EndCamera();
 
@@ -39,6 +222,27 @@ int main() {
         ImGui::SetNextWindowSize({ 500, 800 }, ImGuiCond_Once);
         ImGui::Begin("Panel");
         ImGui::Text("fps = %f", GetFrameRate());
+
+        ImGui::Checkbox("show original data", &showOriginalData);
+
+        ImGui::PushStyleColor(ImGuiCol_Text, IM_COL32(255, 0, 0, 255));
+        ImGui::Checkbox("showX", &showX);
+        ImGui::PopStyleColor();
+
+        ImGui::PushStyleColor(ImGuiCol_Text, IM_COL32(0, 255, 0, 255));
+        ImGui::Checkbox("showY", &showY);
+        ImGui::PopStyleColor();
+
+        ImGui::PushStyleColor(ImGuiCol_Text, IM_COL32(0, 0, 255, 255));
+        ImGui::Checkbox("showZ", &showZ);
+        ImGui::PopStyleColor();
+
+        ImGui::Checkbox("show PDF(y)", &showPDF);
+        if (showPDF)
+        {
+            ImGui::SliderFloat("pdf view scale", &pdf_view_scale, 0, 1);
+        }
+        ImGui::Checkbox("show Sampled Histogram(y)", &showSampledHistogram);
 
         ImGui::End();
 
---
+++ train.py
@@ -0,0 +1,218 @@
+import torch
+import torch.nn as nn
+import csv
+import matplotlib.pyplot as plt
+
+wavelength = []
+Xs = []
+Ys = []
+Zs = []
+with open("lin2012xyz10e_1_7sf.csv", newline="", encoding="utf-8") as f:
+    reader = csv.reader(f)
+    for row in reader:
+        wavelength.append(float(row[0]))
+        Xs.append(float(row[1]))
+        Ys.append(float(row[2]))
+        Zs.append(float(row[3]))
+
+def asymmetric_gaussian( x, mean, sigma, a ):
+    k = (x - mean) / ( sigma + a * (x - mean) )
+    return torch.exp( - k * k )
+
+class AGaussianSinglePeak(nn.Module):
+    def __init__(self, mean_init, peak):
+        super().__init__()
+        self.peak = peak
+        self.mean = nn.Parameter(torch.tensor(mean_init))
+        self.sigma = nn.Parameter(torch.tensor(100.0))
+        self.a = nn.Parameter(torch.tensor(0.0))
+
+    def forward(self, x):
+        return self.peak * asymmetric_gaussian(x, self.mean, self.sigma, self.a)
+    
+    def cFunction(self, name):
+        return f"""
+        inline float asymmetric_gaussian( float x, float mean, float sigma, float a ) {{
+            float k = (x - mean) / ( sigma + a * (x - mean) );
+            return expf( - k * k );
+        }}
+        inline float {name}( float x ) {{
+            return {self.peak}f * asymmetric_gaussian( x, {self.mean.item()}f, {self.sigma.item()}f, {self.a.item()}f );
+        }}
+        """ 
+    
+class AGaussianDualPeak(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.f1 = AGaussianSinglePeak(mean_init=430.0, peak=0.42)
+        self.f2 = AGaussianSinglePeak(mean_init=600.0, peak=1.160)
+        self.c = nn.Parameter(torch.tensor(10.0))
+    def forward(self, x):
+        a = self.f1(x)
+        b = self.f2(x)
+        return a + b - a * b * self.c
+    
+    def cFunction(self, name):
+        return f"""
+        inline float asymmetric_gaussian( float x, float mean, float sigma, float a ) {{
+            float k = (x - mean) / ( sigma + a * (x - mean) );
+            return expf( - k * k );
+        }}
+        inline float {name}( float x ) {{
+            float a = {self.f1.peak}f * asymmetric_gaussian( x, {self.f1.mean.item()}f, {self.f1.sigma.item()}f, {self.f1.a.item()}f );
+            float b = {self.f2.peak}f * asymmetric_gaussian( x, {self.f2.mean.item()}f, {self.f2.sigma.item()}f, {self.f2.a.item()}f );
+            return a + b - a * b * {self.c.item()}f;
+        }}
+        """
+
+def logistic( x, s ):
+    k = torch.exp( - torch.abs(x) / s )
+    return s * k / ( (1.0 + k) ** 2 )
+
+class LogisticPDFSinglePeak(nn.Module):
+    def __init__(self, mean_init, lowerBound, upperBound ):
+        super().__init__()
+        self.sigma = nn.Parameter(torch.tensor(16.0))
+        self.mean = nn.Parameter(torch.tensor(mean_init))
+        self.lowerBound = lowerBound
+        self.upperBound = upperBound
+
+    def forward(self, x):
+        return logistic( x - self.mean, self.sigma )
+    
+    def cFunction(self, name_pdf, name_sample):
+        return f"""
+        inline float logistic_pdf( float x, float s )
+        {{
+            float k = expf( - fabsf(x) / s );
+            return s * k / ( (1.0 + k) * (1.0 + k) );
+        }}
+        inline float logistic_cdf( float x, float s )
+        {{
+            return 1.0f / ( 1.0f + expf( - x / s ) );
+        }}
+        inline float inverse_logistic_cdf( float u, float s ) 
+        {{
+            if (0.99999994f < u) {{ u = 0.99999994f; }}
+            if (u < 1.175494351e-38f) {{ u = 1.175494351e-38f; }}
+            return -s * logf( 1.0f / u - 1.0f );
+        }}
+        inline float trimmed_logistic_pdf( float x, float s, float a, float b )
+        {{
+            return logistic_pdf( x, s ) / ( logistic_cdf(b, s) - logistic_cdf(a, s) );
+        }}
+
+        inline float {name_pdf}( float x ) {{
+            float sx = x - {self.mean.item()}f;
+            float s = {self.sigma.item()}f;
+            float a = {self.lowerBound - self.mean.item()};
+            float b = {self.upperBound - self.mean.item()};
+            return logistic_pdf(sx, {self.sigma.item()}f) / ( logistic_cdf(b, s) - logistic_cdf(a, s) );
+        }}
+
+        inline float {name_sample}( float u ) {{
+            float s = {self.sigma.item()}f;
+            float a = {self.lowerBound - self.mean.item()};
+            float b = {self.upperBound - self.mean.item()};
+            float Pa = logistic_cdf(a, s);
+            float Pb = logistic_cdf(b, s);
+            return inverse_logistic_cdf( Pa + (Pb - Pa) * u, s ) + {self.mean.item()}f;
+        }}
+        """ 
+
+if True:
+    function_x = AGaussianDualPeak()
+    optimizer = torch.optim.Adam(params=function_x.parameters(), lr=0.1, eps=1e-15)
+    loss_fn = torch.nn.MSELoss()
+
+    for i in range(20000):
+        pred = function_x(torch.tensor(wavelength))
+
+        loss = loss_fn(pred, torch.tensor(Xs))
+        
+        optimizer.zero_grad()
+        
+        loss.backward()
+
+        optimizer.step()
+
+    print(function_x.cFunction("cmf_x_CIE_2006_10deg"))
+
+    plt.plot(wavelength, Xs, label='X(reference)')
+    plt.plot(wavelength, pred.detach().numpy(), label='X(fit)')
+    plt.xlabel('Wavelength (nm)')
+    plt.legend()
+    plt.show()
+
+if True:
+    function_y = AGaussianSinglePeak(mean_init=550.0, peak=1.0)
+    optimizer = torch.optim.Adam(params=function_y.parameters(), lr=0.1, eps=1e-15)
+    loss_fn = torch.nn.MSELoss()
+
+    for i in range(10000):
+        pred1 = function_y(torch.tensor(wavelength))
+
+        loss = loss_fn(pred1, torch.tensor(Ys))
+        
+        optimizer.zero_grad()
+        
+        loss.backward()
+
+        optimizer.step()
+
+    print(function_y.cFunction("cmf_y_CIE_2006_10deg"))
+
+    plt.plot(wavelength, Ys, label='Y(reference)')
+    plt.plot(wavelength, pred1.detach().numpy(), label='Y(fit)')
+    plt.xlabel('Wavelength (nm)')
+    plt.legend()
+    plt.show()
+
+if True:
+    function_y_pdf = LogisticPDFSinglePeak(mean_init=550., lowerBound=390, upperBound=830)
+    optimizer = torch.optim.Adam(params=function_y_pdf.parameters(), lr=0.1, eps=1e-15)
+    loss_fn = torch.nn.KLDivLoss()
+
+    for i in range(10000):
+        pred = function_y_pdf(torch.tensor(wavelength))
+
+        loss = loss_fn((pred / pred.sum()).log(), torch.tensor(Ys) / sum(Ys))
+        
+        optimizer.zero_grad()
+        
+        loss.backward()
+
+        optimizer.step()
+
+    print(function_y_pdf.cFunction("cmf_y_pdf_CIE_2006_10deg", "cmf_y_sample_CIE_2006_10deg"))
+
+    peak = function_y_pdf(function_y_pdf.mean)
+    plt.plot(wavelength, Ys, label='Y(reference)')
+    plt.plot(wavelength, pred.detach().numpy() / peak.item(), label='Y(fit)')
+    plt.xlabel('Wavelength (nm)')
+    plt.legend()
+    plt.show()
+
+if True:
+    function_z = AGaussianSinglePeak(mean_init=450.0, peak=max(Zs))
+    optimizer = torch.optim.Adam(params=function_z.parameters(), lr=0.1, eps=1e-15)
+    loss_fn = torch.nn.MSELoss()
+
+    for i in range(10000):
+        pred = function_z(torch.tensor(wavelength))
+
+        loss = loss_fn(pred, torch.tensor(Zs))
+        
+        optimizer.zero_grad()
+        
+        loss.backward()
+
+        optimizer.step()
+
+    print(function_z.cFunction("cmf_z_CIE_2006_10deg"))
+
+    plt.plot(wavelength, Zs, label='Z(reference)')
+    plt.plot(wavelength, pred.detach().numpy(), label='Z(fit)')
+    plt.xlabel('Wavelength (nm)')
+    plt.legend()
+    plt.show()
